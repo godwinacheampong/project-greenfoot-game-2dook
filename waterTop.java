0f964b0b698cf827a67e2645c668c42271015ba9
Godwin Acheampong <302088476@student.rocmondriaan.nl>
2018-11-16 12:09:44 +0100

    Rewrite World1.java

---
+++ waterTop.java
@@ -0,0 +1,22 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class waterTop here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class waterTop extends Tile
+{
+    /**
+     * Act - do whatever the waterTop wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public waterTop(String image, int width, int heigth) {
+        super(image, width, heigth);
+    }
+    public void act() 
+    {
+        
+    }    
+}
